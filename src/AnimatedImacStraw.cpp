6cea3913f2f76c113e9b3cf00d571a103b525bd7
Thomas Pachoud <thomas.pachoud@gmail.com>
2015-01-20 12:12:57 +0100

    delete unused files

---
+++ src/AnimatedImacStraw.cpp
@@ -0,0 +1,31 @@
+//
+//  AnimatedImacStraw.cpp
+//  LaserLiveTool
+//
+//  Created by pach on 14/01/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+
+#include "AnimatedImacStraw.h"
+
+AnimatedImacStraw::AnimatedImacStraw(){
+    
+}
+
+AnimatedImacStraw::~AnimatedImacStraw(){
+    
+}
+
+
+void AnimatedImacStraw::setup() {
+    
+}
+
+void AnimatedImacStraw::update() {
+    
+}
+
+void AnimatedImacStraw::draw() {
+    
+}
---
+++ src/AnimatedImacStraw.h
@@ -0,0 +1,26 @@
+//
+//  AnimatedImacStraw.h
+//  LaserLiveTool
+//
+//  Created by pach on 14/01/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+
+class AnimatedImacStraw{
+    
+public:
+    AnimatedImacStraw();
+    ~AnimatedImacStraw();
+    
+    void setup();
+    void update();
+    void draw();
+    
+private:
+    
+};
