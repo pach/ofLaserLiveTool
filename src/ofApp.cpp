709943494543f0b44e6fbcf6a3eef63e85b93df6
Thomas Pachoud <thomas.pachoud@gmail.com>
2015-08-04 13:11:47 +0200

    bring back ilda2

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,9 +11,9 @@ void ofApp::setup(){
     ilda1.setName("laser.1");
     ilda1.load();
     
-//    ilda2.setup(1);
-//    ilda2.setName("laser.2");
-//    ilda2.load();
+    ilda2.setup(1);
+    ilda2.setName("laser.2");
+    ilda2.load();
     
     animManager1.setup();
     animManager1.setDrawWidth(150);
@@ -22,22 +22,22 @@ void ofApp::setup(){
     animManager1.setGuiOffset(ofVec2f(ilda1.getGuiWidth()+150., 0.));
     animManager1.setName("layer.1");
     
-//    animManager2.setup();
-//    animManager2.setDrawWidth(150);
-//    animManager2.setDrawHeight(150);
-//    animManager2.setDrawOffset(ofVec2f(ofGetWidth()-400., ofGetHeight()-200));
-//    animManager2.setGuiOffset(ofVec2f(ilda1.getGuiWidth()+150., 0.));
-//    animManager2.setName("layer.2");
+    animManager2.setup();
+    animManager2.setDrawWidth(150);
+    animManager2.setDrawHeight(150);
+    animManager2.setDrawOffset(ofVec2f(ofGetWidth()-400., ofGetHeight()-200));
+    animManager2.setGuiOffset(ofVec2f(ilda1.getGuiWidth()+150., 0.));
+    animManager2.setName("layer.2");
 
     ildaTabs.setup();
     ildaTabs.setPosition(0, 0);
     ildaTabs.addCanvas(ilda1.getGui());
-//    ildaTabs.addCanvas(ilda2.getGui());
+    ildaTabs.addCanvas(ilda2.getGui());
     
     animManagerTabs.setup();
     animManagerTabs.setPosition(ilda1.getGuiWidth()+150., 0.);
     animManagerTabs.addCanvas(animManager1.getGui());
-//    animManagerTabs.addCanvas(animManager2.getGui());
+    animManagerTabs.addCanvas(animManager2.getGui());
     
 
     
@@ -53,7 +53,7 @@ void ofApp::update(){
     parseOSC();
     
     animManager1.update();
-//    animManager2.update();
+    animManager2.update();
     
     if (ofGetElapsedTimef()-lastOscTime > oscTimeDelay) {
         
@@ -62,46 +62,46 @@ void ofApp::update(){
     }
     
     ilda1.clear();
-//    ilda2.clear();
+    ilda2.clear();
     
     vector<ofPolyline> polys = animManager1.getPolylines();
     for (int i = 0; i<polys.size(); i++) {
         ilda1.addPoly(polys[i]);
     }
-//    polys.clear();
-//    polys = animManager2.getPolylines();
-//    for (int i = 0; i<polys.size(); i++) {
-//        ilda2.addPoly(polys[i]);
-//    }
+    polys.clear();
+    polys = animManager2.getPolylines();
+    for (int i = 0; i<polys.size(); i++) {
+        ilda2.addPoly(polys[i]);
+    }
     
     ilda1.update();
-//    ilda2.update();
+    ilda2.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
     animManager1.draw();
-//    animManager2.draw();
+    animManager2.draw();
     
     ilda1.draw(ofGetWidth()-200, ofGetHeight()-400, 150, 150);
-//    ilda2.draw(ofGetWidth()-200, ofGetHeight()-200, 150, 150);
+    ilda2.draw(ofGetWidth()-200, ofGetHeight()-200, 150, 150);
 }
 
 void ofApp::exit(){
     ilda1.save();
-//    ilda2.save();
+    ilda2.save();
     animManager1.save();
-//    animManager2.save();
+    animManager2.save();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if (key == 's'){
         ilda1.save();
-//        ilda2.save();
+        ilda2.save();
         animManager1.save();
-//        animManager2.save();
+        animManager2.save();
     }
 }
 
@@ -152,8 +152,8 @@ void ofApp::parseOSC(){
         
 //        cout<<"just received OSC "<<m.getAddress()<<endl;
         animManager1.parseOSC(m);
-//        animManager2.parseOSC(m);
+        animManager2.parseOSC(m);
         ilda1.parseOSC(m);
-//        ilda2.parseOSC(m);
+        ilda2.parseOSC(m);
     }
 }
